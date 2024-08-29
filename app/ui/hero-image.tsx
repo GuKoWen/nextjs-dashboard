bdb031b7688e02cbd817ef329ef9bf37a4e6eaa0
GuKoWen <23songphat2002@gmail.com>
2024-08-29 10:51:32 +0700

    ..

---
+++ app/ui/hero-image.tsx
@@ -0,0 +1,23 @@
+export  function  Mobilehero() {
+  return (
+    <img
+        src="/hero-desktop.png"
+        width={1000}
+        height={760}
+        className="hidden md:block"
+        alt="Screenshots of the dashboard project showing desktop version"
+      />
+  );
+}
+export  function  Desktophero() {
+  return (
+    <img
+    src="/hero-mobile.png"
+    width={560}
+    height={620}
+    className="block md:hidden"
+    alt="Screenshot of the dashboard project showing mobile version"
+  />
+  );
+}
+
